0bd9019a28294ba5dfd81bbe9af0865bcc57a035
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-10-24 21:08:40 -0300

    Create signOut function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,14 +7,16 @@ import { styles } from './styles';
 import LogoSvg from '../../assets/logo.svg';
 
 export function Header() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
   return (
     <View style={styles.container}>
       <LogoSvg />
       <View style={styles.logoutButton}>
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>Sair</Text>
-        </TouchableOpacity>
+        {user && (
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>Sair</Text>
+          </TouchableOpacity>
+        )}
         <UserPhoto imageUri={user?.avatar_url} />
       </View>
     </View>
